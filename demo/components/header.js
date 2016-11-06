20ce2a9bdda58183eed4f71c56174d8982286546
Ganapati V S <vsg.inbox@gmail.com>
2016-11-06 16:44:18 +0530

    Header update

--- demo/components/header.js
+++ demo/components/header.js
@@ -31,7 +31,7 @@ export default class Header extends Component {
                         <button class={{"bttn-jelly": true, "active": animateLogo}}>
                             bttn.css
                         </button>
-                        <h4 class="col-xs-12 animated fadeInUp">Simple buttons for your next awesome project!</h4>
+                        <h4 class="col-xs-12 animated fadeInUp">Awesome buttons for awesome projects!</h4>
                         <div class="row col-xs-12 col-sm-8 col-md-6 col-lg-6 col-md-offset-3
                                     col-lg-offset-3 col-sm-offset-2 col-xs-offset-0 around-xs download-options">
                             <div class="col-xs-6 download-bttn end-xs animated fadeInUp">
