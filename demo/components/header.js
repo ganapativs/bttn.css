a02b5bd2722984cf21ee6c9f8368f2ade137d717
Ganapati V S <ganapati.vs@tracxn.com>
2016-12-29 23:12:16 +0530

    Download .min.css instead of .css

--- demo/components/header.js
+++ demo/components/header.js
@@ -35,7 +35,7 @@ export default class Header extends Component {
                         <div class="row col-xs-12 col-sm-8 col-md-6 col-lg-6 col-md-offset-3
                                     col-lg-offset-3 col-sm-offset-2 col-xs-offset-0 around-xs download-options">
                             <div class="col-xs-6 download-bttn end-xs animated fadeInUp">
-                                <a href="https://raw.githubusercontent.com/ganapativs/bttn.css/master/dist/bttn.css"
+                                <a href="https://raw.githubusercontent.com/ganapativs/bttn.css/master/dist/bttn.min.css"
                                    download="bttn.css">
                                     <button class="bttn-gradient bttn-md bttn-warning">
                                         Download
